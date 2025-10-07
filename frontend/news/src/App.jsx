5a9c0848da925b102f980841eeb1ceba91d65b94
thameenasulaiman <thameenameenu06@gmail.com>
2025-10-07 11:18:30 +0530

    frontend commit

--- frontend/news/src/App.jsx
+++ frontend/news/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import io from "socket.io-client";
 import axios from "axios";
 import "./index.css";
-const socket = io("https://news-project-3.onrender.com"); 
+const socket = io("https://news-project-5.onrender.com"); 
 
 function App() {
   const [email, setEmail] = useState("");
@@ -24,7 +24,7 @@ function App() {
       return;
     }
     try {
-      const response = await axios.post("https://news-project-3.onrender.com/subscribe", {
+      const response = await axios.post("https://news-project-5.onrender.com/subscribe", {
         email,
         categories,
         frequency,
